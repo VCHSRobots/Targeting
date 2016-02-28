709bebc5a1165641374fe94573c5ba2c7b272b68
3265MNE <3265MNE@gmail.com>
2016-02-27 16:37:12 -0800

    Removed "smooth" Ctrl-c handaling for testing.

--- TargetDetection/TargetDetect.py
+++ TargetDetection/TargetDetect.py
@@ -193,170 +193,167 @@ except:
 	CommOutbound("RPiConsole", "[FATL] Unable to start camera! Attempting to continue with program.")
 #Capture frames from the camera
 CommOutbound("RPiConsole", "[INFO] Starting image capturing and processing...")
-try:
-	for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-		if imagecount % 10 == True:
-			#
-			#Runs with settings set for "regular picture mode"
-			#
-			#Start latentcy timer
-			TimeStart = time.time()
-			#
-			#Set values for processing:
-			#
-			#Draw Threshold?
-			drawThresh = 0
-			#Use adaptive threshold?
-			useAdaptive = 1
-			#Set the threshold value
-			thresh = 5
-			#Set the minimum perimiter of a valid target (in pixel length)
-			minPerim = 45
-			#Set the target average pixel value in the image, used to set the shutter speed
-			autoShutter = 40
-			#Set the max angle we can be off for horizontal and vertical sides
-			eps = 5
-			#Set the aspect ratio of the target (width / height)
-			aspect = 1.5
-			#Set the aspect ratio tolerance in percetage
-			aspectTol = 0.5
-			#Set the adaptive threshold size.
-			adaptiveSize = 3
-			#Set the shutter speed.
-			ss = camera.shutter_speed
-		else:
-			#
-			#Runs with settings set for optimal goaldetection conditions
-			#
-			#Start latentcy timer
-			TimeStart = time.time()
-			#
-			#Set values for processing:
-			#
-			#Draw Threshold?
-			drawThresh = 1
-			#Use adaptive threshold?
-			useAdaptive = 1
-			#Set the threshold value
-			thresh = 5
-			#Set the minimum perimiter of a valid target (in pixel length)
-			minPerim = 45
-			#Set the target average pixel value in the image, used to set the shutter speed
-			autoShutter = 40
-			#Set the max angle we can be off for horizontal and vertical sides
-			eps = 5
-			#Set the aspect ratio of the target (width / height)
-			aspect = 1.5
-			#Set the aspect ratio tolerance in percetage
-			aspectTol = 0.5
-			#Set the adaptive threshold size.
-			adaptiveSize = 3
-			#Set the shutter speed.
-			ss = camera.shutter_speed
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	if imagecount % 10 == True:
 		#
-		#Process image
+		#Runs with settings set for "regular picture mode"
 		#
-		#Grab the raw NumPy array representing the image
-		drawnImage = image = frame.array
-		#Convert to a grayscale image
-		gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-		#Calculate the avarage pixel value
-		avg = cv2.mean(gray_image)
-		#Threshold the grayscale image
-		if useAdaptive == 0:
-			#Use the simple global threshold routine
-			ret, threshImg = cv2.threshold(gray_image, thresh, 255, cv2.THRESH_BINARY)
-		else:
-			#Use the fancy adaptive threshold routine
-			threshImg = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, adaptiveSize, thresh)
-		#If value is set to 1, use the threshold image to draw on instead of the original
-		if drawThresh == 1:
-			#Convert the threshold image back to color so we can draw on it with colorful lines
-			drawnImage = cv2.cvtColor(threshImg, cv2.COLOR_GRAY2RGB)
-		#Find the contours in the thresholded image...
-		im2, contours, high = cv2.findContours(threshImg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-		#The arrays of detected targets, they're empty now, but we'll fill them next.
-		finalTargets = []
-		#For each contour we found...
-		for cnt in contours:
-			#Get the convexHull
-			hull = cv2.convexHull(cnt)
-			#Get the perimiter of the hull.
-			perim = cv2.arcLength(hull, True)
-			#Is the the perimiter of the hull is > than the minimum allowed?
-			if perim >= minPerim: 
-				#Approximate the hull:
-				aproxHull = cv2.approxPolyDP(hull, 0.1 * perim, True)
-				#Only add this target if it has 4 verticies
-				if len(aproxHull) == 4:
-					#Check to see if the 4 sides are near horizontal or vertical, and check the aspect ratio
-					if CheckAnglesAndAspect(aproxHull, eps, aspect, aspectTol):
-						#Add the contour to the list of final targets
-						finalTargets.append(aproxHull)
-		#Draw on top of the original image...
-		#Draw all the detected hulls back on the original image (green with a width of 3)
-		cv2.drawContours(drawnImage, finalTargets, -1, blue, 3)
-		#Find the center of each target:
-		for target in finalTargets:
-			#Find it's moments:  
-			M = cv2.moments(target)
-			#Get the center x and y values...
-			cx = int(M['m10']/M['m00'])
-			cy = int(M['m01']/M['m00'])
-			#Get the angles to the target
-			angles = GetAzEl((cx, cy))
-			#Draw the angles on the screen
-			text = '(%.0f,%0.f)' % angles
-			cv2.putText(drawnImage, text, (cx + 5, cy + 5), cv2.FONT_HERSHEY_SIMPLEX, 0.33, green, 1)
-			#Draw a little crosshair at the center of the target
-			cv2.line(drawnImage, (cx-2, cy), (cx+2, cy), red, 1)	# little horizontal line
-			cv2.line(drawnImage, (cx, cy-2), (cx, cy+2), red, 1)	# little vertical line
-		#Draw number of detections found on image
-		text = 'Number of detections: %d' % (len(finalTargets))
-		cv2.putText(drawnImage, text, (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.33, red, 1)
-		#Write the image into the datadump directory
-		imagecount = imagecount + 1
-		imagename = 'TargetImage_' + str(imagecount) + '.jpg'
-		cv2.imwrite(imagename,drawnImage)
-		CommOutbound("RPiConsole", "[INFO] Image captured: " + imagename + ".")
-		#Clear the stream in preparation for the next frame
-		rawCapture.truncate(0)
-		#Calculate the increment for the shutter speed by 1% of it's current value
-		inc = 0
-		if avg[0] > (autoShutter + 2):
-			inc = -max(int(ss * 0.10), 2)  # if it's less than 2 use 2 
-		if avg[0] < (autoShutter - 2):
-			inc = max(int(ss * 0.10), 2)  # if it's less than 2 use 2 
-		#Set the shutter speed
-		camera.shutter_speed = ss + inc
-		#Stop Latentcy timer
-		TimeEnd = time.time()
-		Latentcy = math.ceil((TimeEnd - TimeStart))
-		#Add this times latentcy to AverageLatentcy variable
-		AverageLatentcy += Latentcy
-		if len(finalTargets) == 0:
-			CommOutbound("RPiConsole", "[INFO] No goal was detected in image \"" + imagename + "\".")
-		else:
-			CommOutbound("RPiConsole", "[INFO] " + str(len(finalTargets)) + " targets were detected in \"" + imagename + "\"! Sending information to RoboRio module...")
-			#
-			#SEND INFORMATION TO ROBORIO, AND LOG THAT INFORMATION WAS SENT, ALONG WITH A COPY OF THAT INFORMATION! (Both to console and to log file.)
-			#
-			message = "(" + str(angles) + ")"
-			CommOutbound("RPiConsole", "[INFO] Communication stream: RPi --> RPiConsole: " + message)
-			#CommOutbound("RoboRio", "[INFO] Communication stream: RPi --> RoboRio: " + message)
-		#Reset values for TimeStart, TimeEnd, and latentcy
-		TimeStart = 0
-		TimeEnd = 0
-		Latentcy = 0
-		#Listen for any incoming messages from operator console:
-		#CommInbound("Op", "Time", "50")
-		#if message == "halt":
-		#	break
-		#elif message == "download":
-		#	#Code for transfering of log text data, and images will go HERE.
-		#	print("temp code here, just for syntax reasons while testing")
-except KeyboardInterrupt:
-	pass
+		#Start latentcy timer
+		TimeStart = time.time()
+		#
+		#Set values for processing:
+		#
+		#Draw Threshold?
+		drawThresh = 0
+		#Use adaptive threshold?
+		useAdaptive = 1
+		#Set the threshold value
+		thresh = 5
+		#Set the minimum perimiter of a valid target (in pixel length)
+		minPerim = 45
+		#Set the target average pixel value in the image, used to set the shutter speed
+		autoShutter = 40
+		#Set the max angle we can be off for horizontal and vertical sides
+		eps = 5
+		#Set the aspect ratio of the target (width / height)
+		aspect = 1.5
+		#Set the aspect ratio tolerance in percetage
+		aspectTol = 0.5
+		#Set the adaptive threshold size.
+		adaptiveSize = 3
+		#Set the shutter speed.
+		ss = camera.shutter_speed
+	else:
+		#
+		#Runs with settings set for optimal goaldetection conditions
+		#
+		#Start latentcy timer
+		TimeStart = time.time()
+		#
+		#Set values for processing:
+		#
+		#Draw Threshold?
+		drawThresh = 1
+		#Use adaptive threshold?
+		useAdaptive = 1
+		#Set the threshold value
+		thresh = 5
+		#Set the minimum perimiter of a valid target (in pixel length)
+		minPerim = 45
+		#Set the target average pixel value in the image, used to set the shutter speed
+		autoShutter = 40
+		#Set the max angle we can be off for horizontal and vertical sides
+		eps = 5
+		#Set the aspect ratio of the target (width / height)
+		aspect = 1.5
+		#Set the aspect ratio tolerance in percetage
+		aspectTol = 0.5
+		#Set the adaptive threshold size.
+		adaptiveSize = 3
+		#Set the shutter speed.
+		ss = camera.shutter_speed
+	#
+	#Process image
+	#
+	#Grab the raw NumPy array representing the image
+	drawnImage = image = frame.array
+	#Convert to a grayscale image
+	gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	#Calculate the avarage pixel value
+	avg = cv2.mean(gray_image)
+	#Threshold the grayscale image
+	if useAdaptive == 0:
+		#Use the simple global threshold routine
+		ret, threshImg = cv2.threshold(gray_image, thresh, 255, cv2.THRESH_BINARY)
+	else:
+		#Use the fancy adaptive threshold routine
+		threshImg = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, adaptiveSize, thresh)
+	#If value is set to 1, use the threshold image to draw on instead of the original
+	if drawThresh == 1:
+		#Convert the threshold image back to color so we can draw on it with colorful lines
+		drawnImage = cv2.cvtColor(threshImg, cv2.COLOR_GRAY2RGB)
+	#Find the contours in the thresholded image...
+	im2, contours, high = cv2.findContours(threshImg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+	#The arrays of detected targets, they're empty now, but we'll fill them next.
+	finalTargets = []
+	#For each contour we found...
+	for cnt in contours:
+		#Get the convexHull
+		hull = cv2.convexHull(cnt)
+		#Get the perimiter of the hull.
+		perim = cv2.arcLength(hull, True)
+		#Is the the perimiter of the hull is > than the minimum allowed?
+		if perim >= minPerim: 
+			#Approximate the hull:
+			aproxHull = cv2.approxPolyDP(hull, 0.1 * perim, True)
+			#Only add this target if it has 4 verticies
+			if len(aproxHull) == 4:
+				#Check to see if the 4 sides are near horizontal or vertical, and check the aspect ratio
+				if CheckAnglesAndAspect(aproxHull, eps, aspect, aspectTol):
+					#Add the contour to the list of final targets
+					finalTargets.append(aproxHull)
+	#Draw on top of the original image...
+	#Draw all the detected hulls back on the original image (green with a width of 3)
+	cv2.drawContours(drawnImage, finalTargets, -1, blue, 3)
+	#Find the center of each target:
+	for target in finalTargets:
+		#Find it's moments:  
+		M = cv2.moments(target)
+		#Get the center x and y values...
+		cx = int(M['m10']/M['m00'])
+		cy = int(M['m01']/M['m00'])
+		#Get the angles to the target
+		angles = GetAzEl((cx, cy))
+		#Draw the angles on the screen
+		text = '(%.0f,%0.f)' % angles
+		cv2.putText(drawnImage, text, (cx + 5, cy + 5), cv2.FONT_HERSHEY_SIMPLEX, 0.33, green, 1)
+		#Draw a little crosshair at the center of the target
+		cv2.line(drawnImage, (cx-2, cy), (cx+2, cy), red, 1)	# little horizontal line
+		cv2.line(drawnImage, (cx, cy-2), (cx, cy+2), red, 1)	# little vertical line
+	#Draw number of detections found on image
+	text = 'Number of detections: %d' % (len(finalTargets))
+	cv2.putText(drawnImage, text, (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.33, red, 1)
+	#Write the image into the datadump directory
+	imagecount = imagecount + 1
+	imagename = 'TargetImage_' + str(imagecount) + '.jpg'
+	cv2.imwrite(imagename,drawnImage)
+	CommOutbound("RPiConsole", "[INFO] Image captured: " + imagename + ".")
+	#Clear the stream in preparation for the next frame
+	rawCapture.truncate(0)
+	#Calculate the increment for the shutter speed by 1% of it's current value
+	inc = 0
+	if avg[0] > (autoShutter + 2):
+		inc = -max(int(ss * 0.10), 2)  # if it's less than 2 use 2 
+	if avg[0] < (autoShutter - 2):
+		inc = max(int(ss * 0.10), 2)  # if it's less than 2 use 2 
+	#Set the shutter speed
+	camera.shutter_speed = ss + inc
+	#Stop Latentcy timer
+	TimeEnd = time.time()
+	Latentcy = math.ceil((TimeEnd - TimeStart))
+	#Add this times latentcy to AverageLatentcy variable
+	AverageLatentcy += Latentcy
+	if len(finalTargets) == 0:
+		CommOutbound("RPiConsole", "[INFO] No goal was detected in image \"" + imagename + "\".")
+	else:
+		CommOutbound("RPiConsole", "[INFO] " + str(len(finalTargets)) + " targets were detected in \"" + imagename + "\"! Sending information to RoboRio module...")
+		#
+		#SEND INFORMATION TO ROBORIO, AND LOG THAT INFORMATION WAS SENT, ALONG WITH A COPY OF THAT INFORMATION! (Both to console and to log file.)
+		#
+		message = "(" + str(angles) + ")"
+		CommOutbound("RPiConsole", "[INFO] Communication stream: RPi --> RPiConsole: " + message)
+		#CommOutbound("RoboRio", "[INFO] Communication stream: RPi --> RoboRio: " + message)
+	#Reset values for TimeStart, TimeEnd, and latentcy
+	TimeStart = 0
+	TimeEnd = 0
+	Latentcy = 0
+	#Listen for any incoming messages from operator console:
+	#CommInbound("Op", "Time", "50")
+	#if message == "halt":
+	#	break
+	#elif message == "download":
+	#	#Code for transfering of log text data, and images will go HERE.
+	#	print("temp code here, just for syntax reasons while testing")
 message = "[INFO] Program inturrupt signal detected! Stopping program..."
 CommOutbound("RPiConsole", "[INFO]  " + message)
 #Record average latentcy
